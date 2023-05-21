5ef80f5373c0bd8ac0a98b31aab2d42465466cca
Andrew Bustos <infernious@live.com>
2023-05-21 03:31:26 -0700

    cwd attempt

--- api/views.py
+++ api/views.py
@@ -13,9 +13,9 @@ def process_img(request): #post /api/process
             return res
       file_name = request.FILES['image']
       #file_content = file_name.read()
-      if os.path.dirname(os.getcwd()) != '/darknet':
+      if os.getcwd() != './darknet':
             os.chdir('./darknet')
-      command = 'python3 darknet.py detector test cfg/coco.data cfg/yolov4.cfg yolov4.weights ' + file_name.path
+      command = './darknet detector test cfg/coco.data cfg/yolov4.cfg yolov4.weights ' + file_name.path
       subprocess.run([command])
       newPath = cv.saveImg('predictions.jpg')
       file = None
